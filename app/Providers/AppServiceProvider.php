8ee60dcbc24d4bd21e8cd486e185cd1193e8c4f4
EC2 Default User <ec2-user@ip-172-31-40-4.ap-northeast-1.compute.internal>
2020-04-27 22:37:29 +0000

    modification CXL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (request()->isSecure()) {
-            \URL::forceScheme('https');
-        }
+        
     }
 }
